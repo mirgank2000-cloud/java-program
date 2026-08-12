24118b440a1fc170e4bb84e4a10b6138a86eedca
mirgank <mirgank2000@gmail.com>
2026-08-12 15:52:25 +0530

    student2 file

---
+++ student2.java
@@ -0,0 +1,10 @@
+public class student2 {
+    public static void main(String[] args) {
+        int number1 = 20;
+        int number2 = 30;
+        int mul = (number1 * number2);
+        System.out.println("multiplication" + mul);
+
+    }
+
+}
